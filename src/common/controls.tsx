a87b62baa3a2c38fa1a9112a6c1bb9f3fa1aad9c
lazaralex98 <alex2012ro@gmail.com>
2023-10-06 17:15:32 +0300

    separate getting tx from processing reports

--- src/common/controls.tsx
+++ src/common/controls.tsx
@@ -6,9 +6,12 @@ function Controls() {
         You can use this panel to manage your data.
       </p>
       <button className="mt-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-        Get fresh data
+        Get transactions
       </button>
-      <button className="ml-2 mt-2 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+      <button className="ml-2 mt-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        Process reports
+      </button>
+      <button className="mt-2 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
         Clear data
       </button>
     </div>
