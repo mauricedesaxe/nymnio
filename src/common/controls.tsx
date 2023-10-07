c5f2518a9f0dc7058dfacfbea58902c5958cbb5a
lazaralex98 <alex2012ro@gmail.com>
2023-10-07 13:03:44 +0300

    add network data in transactions

--- src/common/controls.tsx
+++ src/common/controls.tsx
@@ -18,8 +18,19 @@ function Controls() {
     const addresses = addresses$.get();
     const networks = networks$.get();
 
-    const normalTx: z.infer<typeof normalTxSchema>[] = [];
-    const ERC20Tx: z.infer<typeof erc20TxSchema>[] = [];
+    const transactions: {
+      timeStamp: string;
+      hash: string;
+      from: string;
+      to: string;
+      value: string;
+      contractAddress: string;
+      tokenName: string;
+      tokenSymbol: string;
+      tokenDecimal: string;
+      network: string;
+      type: string;
+    }[] = [];
 
     // Only proceed if there are valid addresses and apiKeys
     const validAddresses = addresses.filter(
@@ -73,69 +84,52 @@ function Controls() {
           ]);
         }
 
-        normalTx.push(...localFilteredNormalTx);
-        ERC20Tx.push(...localERC20Tx);
+        // standardize & merge the two into the transactions array
+        for (const tx of localFilteredNormalTx) {
+          transactions.push({
+            timeStamp: tx.timeStamp,
+            hash: tx.hash,
+            from: tx.from,
+            to: tx.to,
+            value: tx.value,
+            contractAddress: tx.contractAddress,
+            tokenName: network.tokenName,
+            tokenSymbol: network.tokenSymbol,
+            tokenDecimal: network.tokenDecimal,
+            network: network.name,
+            type: "normal",
+          });
+        }
+        for (const tx of localERC20Tx) {
+          transactions.push({
+            timeStamp: tx.timeStamp,
+            hash: tx.hash,
+            from: tx.from,
+            to: tx.to,
+            value: tx.value,
+            contractAddress: tx.contractAddress,
+            tokenName: tx.tokenName,
+            tokenSymbol: tx.tokenSymbol,
+            tokenDecimal: tx.tokenDecimal,
+            network: network.name,
+            type: "erc20",
+          });
+        }
       }
     }
 
-    // merge the transactions in a unified interface
-    const mergedTransactions: {
-      timeStamp: string;
-      hash: string;
-      from: string;
-      to: string;
-      value: string;
-      contractAddress: string;
-      tokenName: string;
-      tokenSymbol: string;
-      tokenDecimal: string;
-      network: string;
-    }[] = [];
-    for (const tx of normalTx) {
-      mergedTransactions.push({
-        timeStamp: tx.timeStamp,
-        hash: tx.hash,
-        from: tx.from,
-        to: tx.to,
-        value: tx.value,
-        contractAddress: "",
-        tokenName: "Ether", // TODO Ether needs to be replaced with the native token name of the network
-        tokenSymbol: "ETH", // TODO ETH needs to be replaced with the native token symbol of the network
-        tokenDecimal: "18",
-        network: "Ethereum", // TODO needs extracted from the network
-      });
-    }
-    for (const tx of ERC20Tx) {
-      mergedTransactions.push({
-        timeStamp: tx.timeStamp,
-        hash: tx.hash,
-        from: tx.from,
-        to: tx.to,
-        value: tx.value,
-        contractAddress: tx.contractAddress,
-        tokenName: tx.tokenName,
-        tokenSymbol: tx.tokenSymbol,
-        tokenDecimal: tx.tokenDecimal,
-        network: "Ethereum", // TODO needs extracted from the network
-      });
-    }
-
     // log a few things
-    console.log("mergedTransactions", mergedTransactions);
+    console.log("transactions", transactions);
     logs$.set((logs) => [
       ...logs,
-      `[${new Date()}] Fetched ${normalTx.length} normal transactions`,
-    ]);
-    logs$.set((logs) => [
-      ...logs,
-      `[${new Date()}] Fetched ${ERC20Tx.length} ERC20 transactions`,
+      `[${new Date()}] Fetched ${transactions.length} transactions`,
     ]);
 
     // store the transactions
-    transactions$.set(mergedTransactions);
+    transactions$.set(transactions);
     logs$.set((logs) => [
       ...logs,
-      `[${new Date()}] Stored ${mergedTransactions.length} transactions`,
+      `[${new Date()}] Stored ${transactions.length} transactions`,
     ]);
   }
 
