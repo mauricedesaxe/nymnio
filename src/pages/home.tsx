2c4e760b78af9efc250d2298eab2a3faf3393857
lazaralex98 <alex2012ro@gmail.com>
2023-10-08 00:18:02 +0300

    change copy of local storage clearing

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -85,11 +85,7 @@ export default function Home() {
 
             <button
               onClick={() => {
-                if (
-                  !window.confirm(
-                    "Are you sure you want to clear local storage?"
-                  )
-                ) {
+                if (!window.confirm("Are you sure you want to reset app?")) {
                   return;
                 }
                 localStorage.clear();
@@ -97,7 +93,7 @@ export default function Home() {
               }}
               className="my-2 p-2 text-red-500 hover:text-red-50 hover:bg-red-600 rounded font-medium"
             >
-              Clear local storage
+              Reset app
             </button>
           </aside>
         </div>
